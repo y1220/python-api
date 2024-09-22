2cefb81760f43c3e3b1c9666d89403002ff04d00
y1220 <ga.2.y1220@gmail.com>
2024-09-22 23:41:55 +0200

    chore(dockerConda): remove unnecessary import

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
 from flask import Flask, request
 
-import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.feature_selection import RFECV, SequentialFeatureSelector, SelectFromModel
-from sklearn.inspection import permutation_importance
-from sklearn.metrics import classification_report
-#from sklearn.model_selection import train_test_split, StratifiedKFold
-#from sklearn.neighbors import KNeighborsClassifier
-#from sklearn.svm import SVC
+
 import json
 
 #import analysis.methods.personas_validation_methods as pvm
